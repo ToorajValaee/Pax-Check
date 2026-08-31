b8ffec28b9052707a3fc9083169bc1d1e3038807
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-31 21:27:10 +0330

    Remove unnecessary serialization from navigation keys

--- app/src/main/java/com/example/paxcheck/ui/navigation/NavRoutes.kt
+++ app/src/main/java/com/example/paxcheck/ui/navigation/NavRoutes.kt
@@ -1,15 +1,7 @@
 package com.example.paxcheck.ui.navigation
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 sealed class NavRoutes {
-    @Serializable
     data object Dashboard : NavRoutes()
-
-    @Serializable
     data object MsrTest : NavRoutes()
-
-    @Serializable
     data object PrinterTest : NavRoutes()
 }
